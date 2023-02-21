6c525770cb5efb54e6308448c5c8d9f826581ab6
JoyceHuang <joycehuang@gmail.com>
2023-02-21 16:41:28 +0800

    加入專案檔案。

---
+++ EmployeeService/Program.cs
@@ -0,0 +1,34 @@
+using EmployeeService.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the DI container. Dependency Injection(DI)注入:外部傳入
+
+var NorthwindconnectionString = builder.Configuration.GetConnectionString("Northwind");
+builder.Services.AddDbContext<NorthwindContext>(options => 
+    options.UseSqlServer(NorthwindconnectionString));
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
